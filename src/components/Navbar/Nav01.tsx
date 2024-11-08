67f9abb73f7dc244387ab1ec74844ea2ba820dbb
NattakarnPreesong <nattakarnpreesong@gmail.com>
2024-11-08 13:54:41 +0700

    update

--- src/components/Navbar/Nav01.tsx
+++ src/components/Navbar/Nav01.tsx
@@ -1,23 +1,29 @@
 "use client";
 
 import React from "react";
+import { HiOutlineSearch } from "react-icons/hi";
 
 function Nav01() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center bg-white p-4 shadow-md rounded-lg">
       {/* Logo */}
-      <img src="/images/Logo.png"
+      <img
+        src="/images/Logo.png"
         alt="Logo"
         className="drop-shadow-sm"
         width="60px"
         height="auto"
       />
+
       {/* Search Bar */}
-      <input
-        type="text"
-        placeholder="ค้นหาสถานที่"
-        className="flex-grow p-2 rounded-fullbg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none"
-      />
+      <div className="relative flex-grow ml-4">
+        <input
+          type="text"
+          placeholder="ค้นหาสถานที่"
+          className="w-full rounded-full py-2 pl-4 pr-10 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <HiOutlineSearch className="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500" />
+      </div>
     </div>
   );
 }
