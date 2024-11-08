2fac49c48915642faf534f24fa047254cf0ee939
NattakarnPreesong <nattakarnpreesong@gmail.com>
2024-11-08 13:46:23 +0700

    test

--- public/components/Navbar/Nav01.tsx
+++ public/components/Navbar/Nav01.tsx
@@ -6,10 +6,9 @@ function Nav01() {
   return (
     <div className="flex items-center">
       {/* Logo */}
-      <img
-        src="/images/logo.png" // เปลี่ยนเป็น path ของโลโก้ของคุณ
+      <img src="/images/Logo.png"
         alt="Logo"
-        className="w-10 h-10 mr-3"
+        className="drop-shadow-sm"
         width="60px"
         height="auto"
       />
@@ -17,7 +16,7 @@ function Nav01() {
       <input
         type="text"
         placeholder="ค้นหาสถานที่"
-        className="flex-grow p-2 rounded-full bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none"
+        className="flex-grow p-2 rounded-fullbg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none"
       />
     </div>
   );
